45df31f4ffaf67e3fd60c6cd3afa8ff3ad58198f
noboru-i <ishikura.noboru@gmail.com>
2015-12-10 16:44:57 +0900

    test swiftlint

--- fastlane/actions/swiftlint.rb
+++ fastlane/actions/swiftlint.rb
@@ -1,11 +1,28 @@
 module Fastlane
   module Actions
     class SwiftlintAction < Action
+      REPORTERS = ['xcode', 'json', 'csv']
+
       def self.run(params)
         if `which swiftlint`.to_s.length == 0 and !Helper.test?
-          raise "You have to install swiftlint using `brew install swiftlint`".red
+          fail 'You have to install swiftlint using `brew install swiftlint`'.red
         end
-        Actions.sh("swiftlint")
+
+        require 'yaml'
+        config_file = '.swiftlint.yml'
+        FileUtils.cp(config_file, "#{config_file}.back", preserve: true)
+        lint_config = File.exist?(config_file) ? YAML.load_file(config_file) : []
+        lint_config['reporter'] = params[:reporter]
+        open config_file, 'w' do |f|
+          YAML.dump(lint_config, f)
+        end
+
+        command = 'swiftlint'
+        command << " > #{params[:output_file]}" if params[:output_file]
+        Actions.sh(command)
+
+        FileUtils.cp("#{config_file}.back", config_file, preserve: true)
+        FileUtils.rm("#{config_file}.back")
       end
 
       #####################################################
@@ -13,7 +30,7 @@ module Fastlane
       #####################################################
 
       def self.description
-        "Run swift code validation using SwiftLint"
+        'Run swift code validation using SwiftLint'
       end
 
       def self.details
@@ -21,6 +38,16 @@ module Fastlane
 
       def self.available_options
         [
+          FastlaneCore::ConfigItem.new(key: :output_file,
+                                       description: 'Path to output SwiftLint result',
+                                       optional: true),
+          FastlaneCore::ConfigItem.new(key: :reporter,
+                                       description: 'Custom reporter of SwiftLint result',
+                                       optional: true,
+                                       default_value: 'xcode',
+                                       verify_block: proc do |value|
+                                         fail 'Unknown reporter' unless REPORTERS.include?(value)
+                                       end)
         ]
       end
 
@@ -31,7 +58,7 @@ module Fastlane
       end
 
       def self.authors
-        ["KrauseFx"]
+        ['KrauseFx', 'noboru-i']
       end
 
       def self.is_supported?(platform)
